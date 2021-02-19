a0fc7d1ce940bf823c34976f5d6c240851b01fa7
Will B <wberman27@gmail.com>
2021-02-19 11:20:34 -0500

    added structuring 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import styled from 'styled-components'
 import './App.css';
+import {BASE_URL} from './constants/index'
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +12,30 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([])
+  const [currentCharacterId, setCurrentCharacterId] = useState(null)
+
+  const open = id =>{
+    setCurrentCharacterId(id)
+  }
+
+  const close = () =>{
+    setCurrentCharacterId(null)
+  }
+
+  useEffect(() =>{
+    const fetchCharacters = () =>{
+      axios
+      .get()
+      .then(res =>{
+        setCharacters(res.data);
+      })
+      .catch(err =>{
+        console.log(err)
+      });
+    };
+    fetchCharacters();
+  }, []);
 
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import {ThemeProvider} from 'styled-components'
 import App from "./App";
+import theme from './theme'
 
 import { worker } from "./mocks/browser";
 worker.start();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <ThemeProvider theme={theme}><App /></ThemeProvider>,
+    document.getElementById("root")
+    
+);
